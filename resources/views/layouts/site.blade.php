0c0b7e8049b5140a46bbf396ee9859b0c138af8e
Developer <je.contrerass@duocuc.cl>
2026-04-16 21:49:00 -0400

    Icono pestaña

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Thiago Barber')</title>
-    <link rel="icon" type="image/svg+xml" href="{{ asset('images/icon.svg') }}">
+    <link rel="icon" type="image/jpeg" href="{{ asset('images/iconnpng.jpeg') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Cormorant+Garamond:ital,wght@0,400;0,500;0,700;1,600&family=Manrope:wght@400;500;700;800&display=swap" rel="stylesheet">
